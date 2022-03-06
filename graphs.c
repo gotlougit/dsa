2bfbd22794d3edef3eeb2f6c2e3d00cb3c1760ef
Saksham Mittal <gotlouemail@gmail.com>
2022-03-06 10:35:06 +0530

    Create "ivalue" variable in graph to denote where adj list ends

--- graphs.c
+++ graphs.c
@@ -5,27 +5,27 @@ void main(void) {
 	int n = 4;
 	struct vertex *graph = (struct vertex *) malloc(n * sizeof(struct vertex));
 
-	insertVertex(&graph[0], 0);
-	insertVertex(&graph[0], 3);
-	insertVertex(&graph[0], 4);
-	insertVertex(&graph[0], 2);
-	insertVertex(&graph[0], 1);
+	insertVertex(&graph[0], 0, false);
+	insertVertex(&graph[0], 3, true);
+	insertVertex(&graph[0], 4, true);
+	insertVertex(&graph[0], 2, true);
+	insertVertex(&graph[0], 1, true);
 
-	insertVertex(&graph[1], 0);
-	insertVertex(&graph[1], 3);
-	insertVertex(&graph[1], 1);
-	insertVertex(&graph[1], 2);
+	insertVertex(&graph[1], 0, false);
+	insertVertex(&graph[1], 3, true);
+	insertVertex(&graph[1], 1, true);
+	insertVertex(&graph[1], 2, true);
 
-	insertVertex(&graph[2], 0);
-	insertVertex(&graph[2], 2);
-	insertVertex(&graph[2], 4);
-	insertVertex(&graph[2], 1);
-	insertVertex(&graph[2], 3);
+	insertVertex(&graph[2], 0, false);
+	insertVertex(&graph[2], 2, true);
+	insertVertex(&graph[2], 4, true);
+	insertVertex(&graph[2], 1, true);
+	insertVertex(&graph[2], 3, true);
 
-	insertVertex(&graph[3], 0);
-	insertVertex(&graph[3], 1);
-	insertVertex(&graph[3], 3);
-	insertVertex(&graph[3], 4);
+	insertVertex(&graph[3], 0, false);
+	insertVertex(&graph[3], 1, true);
+	insertVertex(&graph[3], 3, true);
+	insertVertex(&graph[3], 4, true);
 
 	printGraph(graph, n);
 
--- graphs.h
+++ graphs.h
@@ -10,6 +10,7 @@
 /*Single node and neighbors stored here*/
 typedef struct vertex {
 
+	int ivalue;
 	int value;
 	bool explored;
 	struct vertex *next;
@@ -18,12 +19,13 @@ typedef struct vertex {
 
 /* Basic insertion, deletion operations for graph*/
 
-void insertVertex(vertex *vgraph, int value) {
+void insertVertex(vertex *vgraph, int value, bool end) {
 
 	vertex *newvertex = (vertex *) malloc(sizeof(vertex));
 
 	if (newvertex != NULL) {
 		*newvertex = *vgraph;
+		vgraph->ivalue = (int) (end);
 		vgraph->value = value;
 		vgraph->explored = false;
 		vgraph->next = newvertex;
@@ -37,7 +39,7 @@ void printGraph(vertex *graph, int n) {
 
 	for (int i = 0; i < n; i++) {
 		int j = 0;
-		for (vertex x = graph[i]; x.value != 0; x = *x.next) {
+		for (vertex x = graph[i]; x.ivalue != 0; x = *x.next) {
 			printf("%d ", x.value);	
 			j++;
 		}
